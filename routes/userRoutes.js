8329a8d691a12751c45c6a84c2994e794d86485b
Caliph Ferguson <caliphferguson@gmail.com>
2024-09-30 11:37:06 -0400

    connection, routes and date format.js

---
+++ routes/userRoutes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+const { 
+    getUsers, 
+    getSingleUser, 
+    createUser, 
+    updateUser, 
+    deleteUser 
+} = require('../controllers/userController');
+
+router.route('/api/users').get(getUsers).post(createUser);
+
+router.route('/api/users/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+module.exports = router;
