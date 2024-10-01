7bb191cf6db9e28a417a10ef33c6fa60384e213d
Caliph Ferguson <caliphferguson@gmail.com>
2024-10-01 00:21:56 -0400

    updated files with the copy and paste

---
+++ controllers/userController.js
@@ -0,0 +1,36 @@
+const { User } = require('../models');
+
+module.exports = {
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select('-__v')
+      .then((user) => 
+        !user ? res.status(404).json({ message: 'No user with that ID' }) : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user with that ID' });
+      }
+
+      await Application.deleteMany({ _id: { $in: user.applications } });
+      res.json({ message: 'User and associated apps deleted!' })
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
-const db = require('./config/connection');
-const routes = require('./routes');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(routes);
+app.use(express.urlencoded({ extended: true }));
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+mongoose.connect('mongodb://localhost/socialNetworkDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+
+mongoose.set('debug', true);
+
+app.use(require('./routes'));
+
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
+
