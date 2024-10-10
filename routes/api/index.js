8615651ef9499b1b4e5619fdea84c84e475567a0
Caliph Ferguson <caliphferguson@gmail.com>
2024-10-10 09:59:40 -0400

    changed index.js route

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const thoughtRoutes = require('./thought-routes');
 
-router.use('/', userRoutes);
-router.use('/thoughts', thoughtRoutes);
+router.use('./api/user', userRoutes);
+router.use('./api/thought', thoughtRoutes);
 
 module.exports = router;
