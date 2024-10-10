4f101d235795750379014b41e3c0c0a37a1c3608
Caliph Ferguson <caliphferguson@gmail.com>
2024-10-10 10:12:51 -0400

    comment out cors

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+// app.use(cors());
 
 mongoose.connect('mongodb://localhost/socialNetworkDB', {
   useNewUrlParser: true,
