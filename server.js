483ee61f14b59785adc433ce5aad6bd38299981a
Caliph Ferguson <caliphferguson@gmail.com>
2024-10-14 08:20:37 -0400

    working site

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const express = require('express');
-const mongoose = require('mongoose');
-// const cors = require('cors');
-const app = express();
+const db = require('./config/connection');
+const routes = require('./routes');
+
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
-// app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-mongoose.connect('mongodb://localhost/socialNetworkDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
 });
 
-mongoose.set('debug', true);
-app.use(require('./routes'));
-app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
 
